42454be9d7e488b898999682f9de595eae6a9ddd
Mark Stringer <mark.stringer@mumbly.co.uk>
2015-07-01 11:36:55 +0100

    Add blog.pl

---
+++ blog.pl
@@ -0,0 +1,227 @@
+use strict;
+use warnings;    
+use Text::Markdown 'markdown';
+use Net::FTP;
+use File::Copy;
+use Net::Twitter;
+use Scalar::Util 'blessed';
+use URI::Escape;
+use XML::RSS::SimpleGen;
+use HTML::Entities;
+use Getopt::Long;
+
+## decide whether we're on windows or linux
+my $OperatingSystem= $^O;
+my $slash;
+
+if ($OperatingSystem =~ /linux/)
+{
+    $slash='/';
+}
+else
+{
+    $slash='\\';
+}
+
+
+my $tweet='';
+my $username='';
+my $password='';
+my $BLOG_DIRECTORY = '.';
+my $PUBLIC_DIRECTORY = "..".$slash."Public";
+my $BLOG_URL = q<http://www.mumbly.co.uk/newblog/>;
+my $FTP_BLOG_DIRECTORY = "newblog";
+my $STYLE_SHEET="blogstyle.css";
+my $RSS_FILE = "feed.rss";
+my $BLOG_TITLE = "A Commonplace";
+my $RSS_FEED = $BLOG_URL.$RSS_FILE;
+my $RSS_ICON = "rss-icon-small.png";
+
+
+GetOptions ('username=s' => \$username, 'password=s' => \$password, 'tweet' => \$tweet );
+print "Username $username\n";
+print "Password $password\n";
+print "Tweet $tweet\n";
+
+opendir (DIR,  $BLOG_DIRECTORY) or die $!;
+
+my $header = <<HEAD;
+<html>
+<head>
+<title>
+$BLOG_TITLE
+</title>
+<meta charset='utf-8'>
+<link href='$STYLE_SHEET' rel='stylesheet'>
+</head>
+<body id="main_body">
+<div id="content">
+<h3>$BLOG_TITLE</h3>
+<span id="commonplace"><p><a href="http://en.wikipedia.org/wiki/Commonplace_book">What is a commonplace?</a></p></span>
+HEAD
+my $nav_frag1 = "<span id=\"link\">";
+my $nav_frag2 = "</span>";
+my $index_frag = $nav_frag1."<a href=\"/".$FTP_BLOG_DIRECTORY."\">Index</a>".$nav_frag2;
+my $link_frag1 = "<a href=\"";
+my $link_frag2 = "\">";
+my $link_frag3 = "</a>";
+my $ul_frag1 ="<ul id=\"index_ul\">";
+my $ul_frag2 ="</ul>";
+my $li_frag1 = "<li>";
+my $li_frag2 = "</li>";
+my $date_frag1="<h3>";
+my $date_frag2="</h3>";
+my $rss_frag = <<RSS;
+<a href='$RSS_FEED' target="_blank"><img alt="RSS Icon" src='$RSS_ICON' title="RSS Feed" border="0"></a>
+RSS
+
+my $index_string="";
+
+my $footer = <<FOOT;
+</div>
+</body>
+</html>
+FOOT
+
+my @file_list = $BLOG_DIRECTORY.$slash.$STYLE_SHEET;
+my $dir = $BLOG_DIRECTORY;
+## Sort the files chronoligcally
+opendir (DIR, $dir);
+     my @dir=readdir(DIR);
+        closedir(DIR);
+         @dir = sort { -M "$dir/$a" <=> -M "$dir/$b" } (@dir);
+	
+my @text_files;
+my @text_file_prefixes;
+my @image_files;
+
+foreach my $file (@dir)
+{
+	if ($file=~ /(.*)\.txt$/)
+	{
+		@text_file_prefixes = (@text_file_prefixes, $1);
+		@text_files = (@text_files, $file);
+	}
+	if ($file=~/(.*)\.(png|jpg|gif)$/)
+	{
+		@image_files = (@image_files, $file);
+	}		
+}
+my $file_index = 0;
+
+foreach my $file (@text_files)
+{		
+		$file=~ /(.*)\.txt$/;
+		my $file_prefix = $1;
+		## read in the whole file
+		 local $/=undef;
+		 $file = $BLOG_DIRECTORY.$slash.$file;
+		 
+		 ##get the date that the file was last accessed
+		  my $modified = (stat($file))[9];
+		  my ($d,$m,$y) = (localtime($modified))[3,4,5];
+		  my $mdy = sprintf '%d/%d/%d', $d, $m+1,  $y+1900;
+		  ##print $mdy." \n";
+		 
+		 open FILE,  $file or die "Couldn\'t open file: $file";
+		 my $string = <FILE>;
+		 close FILE;
+		 
+		 ## encode all the entities
+		 my	 $html = encode_entities($string, '^\n\x20-\x25\x27-\x7e\x00-\x08\x0B\x0C\x0E-\x1F\x80-\x{1FFFFF}');
+		## encode_entities($string);
+		##my	 $html =	encode_entities($string, "\200-\377");
+		## convert to markdown
+		$html = markdown($html);
+		 
+		 my $html_file = $BLOG_DIRECTORY.$slash.$file_prefix."\.htm";
+		 $index_string = $index_string.$li_frag1.$link_frag1.$file_prefix."\.htm".$link_frag2.$file_prefix.$link_frag3.$li_frag2;
+		 
+		 ## create an html file
+		 open FILE, ">"."$html_file" or die "Cannot create file: $!";
+		 print FILE $header;
+		 print FILE $index_frag;
+		 if ($file_index > 0)
+		 {
+			 print FILE $nav_frag1.$link_frag1.$text_file_prefixes[$file_index-1].".htm".$link_frag2."Next".$link_frag3.$nav_frag2;
+		 }
+		 if ($file_index < $#text_files)
+		 {
+			  print FILE $nav_frag1.$link_frag1.$text_file_prefixes[$file_index+1].".htm".$link_frag2."Previous".$link_frag3.$nav_frag2;
+		 }
+		 ## write the date
+		 print FILE $date_frag1.$mdy.$date_frag2;
+		 print FILE $html.$footer;
+		 push (@file_list, $html_file);
+		 close FILE;
+		 $file_index++;
+}
+
+
+## create and index file
+open FILE, ">".$BLOG_DIRECTORY.$slash."index.htm";
+print FILE $header.$ul_frag1.$index_string.$ul_frag2.$rss_frag.$footer;
+close FILE;
+push (@file_list, $BLOG_DIRECTORY.$slash."index.htm");
+## add the image files
+@file_list = (@file_list, @image_files);
+
+## ftp everything up to the blog site
+my $ftp = Net::FTP->new("www.mumbly.co.uk", Debug => 0)
+or die "Cannot connect to some.host.name: $@";
+$ftp->login("strimaa",'4at7EVAD')
+or die "Cannot login ", $ftp->message;
+
+$ftp->cwd("/".$FTP_BLOG_DIRECTORY )
+or die "Cannot change working directory ", $ftp->message;
+$ftp->binary;
+
+foreach my $put_file (@file_list)
+{
+      print $put_file."\n";
+      
+      $ftp->put($put_file) or die "failed to put file $put_file";
+}
+ (my $sec, my$min, my $hour,my $mday, my $mon, my $year, my $wday, my $yday, my $isdst) =
+localtime(time);
+my @abbr = qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec);
+## make an entry in the dropbox public folder, so we can do a post on IFTTT 
+open FILE, ">$PUBLIC_DIRECTORY$slash$year$mon$mday$sec\.txt";
+print FILE $header.$ul_frag1.$index_string.$ul_frag2.$footer;
+close FILE;
+
+## screen  scrape the RSS
+rss_new( $BLOG_URL, "A Commonplace", "Thoughts about Agile and all Manner of Other Things" );
+rss_language( 'en' );
+rss_webmaster( 'mark.stringer@mumbly.co.uk' );
+rss_twice_daily();
+get_url( $BLOG_URL );
+
+##while(
+## just put the most recent article in the RSS feed
+m{<li><a href="(.*?)">(.*?)</a></li>}sg;
+## <li><a href="Hungry Ghost.htm">Hungry Ghost</a></li>
+##) {
+
+my $content_file = $1;
+my $content_title = $2;
+local $/=undef;
+
+open CONTENT, "<$content_file" || die "Failed to open $content_file";
+my $content = <CONTENT>;
+rss_item($BLOG_URL.uri_escape($content_file), $content_title, $content);
+close CONTENT;
+##}
+die "No items in this content?! {{\n$_\n}}\nAborting"
+unless rss_item_count();
+rss_save( $RSS_FILE, 45 );
+##put this file on the server
+$ftp->put($RSS_FILE)||die "failed to put $RSS_FILE";
+
+
+$ftp->quit;
+
+if($tweet)
+{
+   ## don't do anything just now until I figure out how to keep the twitter keys secret
+}
